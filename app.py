c409353dda8acb25ddb8e9f40dab3f9bfa5982e7
Dev-Nebe <cp.nebe@gmail.com>
2020-02-12 21:35:41 +0100

    Feature: Initial migration of data models.
    Created data models for Shows, Artists, and Venues.

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,42 +21,53 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# TODO: connect to a local postgresql database
+# Done TODO: connect to a local postgresql database
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
-
+    __tablename__ = 'venues'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False) #How to handle input of multiple genres?
+
+    # seeking_talent = db.Column(db.String(120), nullable=False)
+    # recruitment_message = db.Column(db.String(120), nullable=False)
+    # image_link = db.Column(db.String(500), nullable=False)
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
-
+    __tablename__ = 'artists'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    # image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__='shows'
+  id = db.Column(db.Integer, primary_key=True)
+  artist_id = db.Column(db.Integer, nullable=False)
+  venue_id = db.Column(db.Integer, nullable=False)
+  start_time = db.Column(db.String(), nullable=False) # Fix the data type for this
+  # Also use the start time to determine if the shows are upcoming or past (in the appropriate controller)
+
 
 #----------------------------------------------------------------------------#
 # Filters.
--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+USERNAME = os.getenv('FYYUR_USERNAME')
+PASSWORD = os.getenv('PASSWORD')
 
 # Enable debug mode.
 DEBUG = True
@@ -10,4 +18,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = f'postgres://{USERNAME}:{PASSWORD}@localhost:5432/fyyur-events'
